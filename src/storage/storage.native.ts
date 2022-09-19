6f0b58570ac218add1f3f389305dbe640cc5938f
Bob Wei <bob61016@gmail.com>
2022-09-19 14:42:40 +0800

    Use @react-native-community/async-storage

--- src/storage/storage.native.ts
+++ src/storage/storage.native.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import { Event } from './event';
 import { Data, StorageEvents, StorageItem, StorageProvider } from './types';
 
